753bf343784963b68d1071617cb78b9d8c6ca075
Davis <davisperchik@gmail.com>
2019-04-16 10:33:37 -0400

    ObjectOrientation done

--- src/main/java/rocks/zipcode/io/objectorientation/TVChannel.java
+++ src/main/java/rocks/zipcode/io/objectorientation/TVChannel.java
@@ -1,17 +1,37 @@
 package rocks.zipcode.io.objectorientation;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public enum TVChannel {
-    ADD_ENUMERATIONS_HERE;
+    MSN(0),
+    CNN(1),
+    FOX(2),
+    AE(3),
+    MTV(4),
+    BET(5),
+    HBO(6),
+    NBC(7),
+    CBS(8);
+    final int ordinal;
 
+    private TVChannel(int ordinal){
+        this.ordinal = ordinal;
+    }
+
+    static Map<Integer, TVChannel> lookup = null;
     /**
      * @param ordinal; ordinal of the expected enumeration
      * @return enumeration with respective ordinal
      */
     public static TVChannel getByOrdinal(Integer ordinal) {
-        return null;
+        if (lookup == null) {
+            lookup = Arrays.stream(TVChannel.values())
+                    .collect(Collectors.toMap(s -> s.ordinal, s -> s));
+        }
+        return lookup.get(ordinal);
     }
 
     /**
--- src/main/java/rocks/zipcode/io/objectorientation/Television.java
+++ src/main/java/rocks/zipcode/io/objectorientation/Television.java
@@ -1,13 +1,27 @@
 package rocks.zipcode.io.objectorientation;
 
 public class Television {
+
+    private TVChannel tvChannel;
+    private Boolean turnedOn;
+
+    public Television(){
+        tvChannel = null;
+        turnedOn = false;
+    }
+
     public void turnOn() {
+        turnedOn = true;
     }
 
     public void setChannel(Integer channel) {
+        if (turnedOn == true) {
+            tvChannel = TVChannel.getByOrdinal(channel);
+        }
+        else throw new IllegalStateException();
     }
 
     public TVChannel getChannel() {
-        return null;
+        return tvChannel;
     }
 }
